178dbc0c715cb232db8bbd5aea317c166fb3bef0
Damien <damien.agopian@insa-lyon.fr>
2018-01-28 12:32:16 +0100

    Commentaires de Chloé

--- serveur_ui.py
+++ serveur_ui.py
@@ -3,25 +3,12 @@ from socket import*
 import time
 import threading
 from random import *
-from  dico import * #Les dictionnaires contenant les caractères japonais
+from  dico import *  # Les dictionnaires contenant les caractères japonais
 import sys
-
-def partition(string):
-	list_word = []
-	word = ''
-	for char in string :
-		word = word+char
-		if char ==';' :
-			list_word.append(word)
-			word = ''
-	return list_word
-
 
 
 class serveur():
 	def __init__(self) :
-
-
 		self.serveur = socket(AF_INET,SOCK_STREAM)
 		self.serveur.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
 
@@ -30,11 +17,12 @@ class serveur():
 
 		self.list_client = []
 		self.list_joueur = []
-		self.joueur_score = [] #Tuple joueur/Score
+		self.joueur_score = []  # Tuple joueur,score
 		self.list_pseudo = []
-		
+
+	# MINI JEU EN 1 CONTRE 1
 	def jeu(self,client) :
-		pseudo = client.recv(1024) #Recoit le pseudo
+		pseudo = client.recv(1024)  # Reçoit le pseudo
 		client.send("Vous êtes connecté au mini jeu 1v1 !;")
 		
 		self.list_joueur.append(client)
@@ -43,55 +31,52 @@ class serveur():
 		
 		while len(self.list_joueur) != 2 :
 			client.send("En attente d'un adversaire...;")
-			#client.recv(1024) #recoit ok / 0
+			# client.recv(1024)  # Reçoit ok / 0
 
 		client.send('Adversaire trouvé !;')
 		
 		game='!'
-		while game != 'fini' :
-			#Attend que le jeu soit fini coté client
+		while game != 'fini' :  # Attend que le jeu soit fini coté client
 			game = client.recv(1024)
 			
 		
-		score = client.recv(1024)#Recois le score
-		self.joueur_score.append((pseudo,int(score)))#Tuple joueur/Score
+		score = client.recv(1024)  # Reçoit le score
+		self.joueur_score.append((pseudo,int(score)))  # Tuple joueur,score
 		
-		while True :
-			#Tant que les deux joueurs n'ont pas fini le jeu...
+		while True :  # Tant que les deux joueurs n'ont pas fini le jeu...
 			if len(self.joueur_score) >= 2 :
-				break 
+				break
+
+		# On détermine le gagnant en comparant les scores
 		if self.joueur_score[0][1] > self.joueur_score[1][1] :
 			gagnant = self.joueur_score[0][0]
-
 		elif self.joueur_score[0][1] < self.joueur_score[1][1] :
 			gagnant = self.joueur_score[1][0]
 		else :
-			gagnant = "Egalité !"
+			gagnant = "Égalité !"
 		
-		gagnant=str(gagnant)
-		client.send("Le gagnant est %s"%gagnant)
+		gagnant = str(gagnant)
+		client.send("Le gagnant est %s :)" %gagnant) # Annonce le gagnant
 		
-		
-		self.joueur_score = [] #On vide la liste
+		# On réinitialise :
+		self.joueur_score = []
 		self.list_joueur = []
 		#client.close()
 		#self.list_client.remove(client)
 		self.list_pseudo.remove(pseudo)
 		choix(self,client)
 		
-
+	# Gère les adversaires du mini-jeu 1V1
 	def multi_joueur(self,client) :
-	#MINI JEU EN 1 CONTRE 1
 		print ('hello')
-		pseudo = client.recv(1024) #Recoie le pseudo du joueur
+		pseudo = client.recv(1024)  # Recoit le pseudo du joueur
 		pseudo = pseudo.decode()
 		print(pseudo)
 		
 		self.list_pseudo.append(pseudo)
 		print(self.list_pseudo)
 		if len(self.list_pseudo) == 1 :
-			client.send("attendre".encode()) 
-			#On dit au client d'attendre un autre joueur
+			client.send("attendre".encode())  # Attente d'un autre joueur
 			attendre = True
 			while attendre == True  :
 				if len(self.list_pseudo) >= 2 :
@@ -99,25 +84,22 @@ class serveur():
 			client.send("commencer".encode())
 			self.choix(client)
 			
-		elif len(self.list_pseudo) > 2 :
+		elif len(self.list_pseudo) > 2 :  # Pas plus de deux joueurs
 			client.send("serveur_plein".encode())
 			self.list_pseudo.remove('pseudo')
 			self.choix(client)
-			#On ne peut pas avoir plus de deux joueurs
 			
-		else :
-			
+		else :  # Si tout est bon, commencer !
 			client.send("commencer".encode())
 			self.choix(client)
 			
+	# Compare les deux score pour donner un gagnant
 	def calcul_gagnant(self,client) :
-		#Compare les deux score pour donner un gagnant
-		score = client.recv(1024) #Recoie le pseudo du joueur
+		score = client.recv(1024)  # Recoit le pseudo du joueur
 		score = score.decode()
 		self.list_score.append(score)
-		#print(self.list_score)
-		#On empeche le calcul du score tant qu'on a pas recu le 
-		#score des deux joueurs
+		# print(self.list_score)
+		# Pas de calcul du score tant qu'on n'a pas recu le score des 2 joueurs
 		while attendre == True  :
 			if len(self.list_score) >= 2 :
 				attendre = False
@@ -130,10 +112,8 @@ class serveur():
 		self.list_score = []
 		self.list_pseudo = []
 		self.choix(client)
-			
-		
 
-
+	# Menu de choix
 	def choix(self,client) :
 		while True :
 			try :
@@ -141,68 +121,68 @@ class serveur():
 				print(rep)
 				if rep.decode() == "nouveauHiragana" :
 					solution = choice(hiragana_)
-					msg = hiragana2[solution] #Envoie d'un nouveau caractère
+					msg = hiragana2[solution]  # Envoi d'un nouveau hiragana
 					client.send(msg.encode())
 					print("Nouveau hiragana envoyé")
 					reponse = client.recv(1024).decode()
 					print(reponse)
-					if reponse != 'STOP': #Si on recoie 'STOP' c'est que le client change d'exercice
+					if reponse != 'STOP':  # Le client change d'exercice
 						if reponse == solution :
 							client.send("VRAI".encode())
 						else :
 							client.send(solution.encode())
 				if rep.decode() == "nouveauKatakana" :
 					solution = choice(katakana_)
-					msg = katakana2[solution] #Envoi d'un nouveau katakana
+					msg = katakana2[solution]  # Envoi d'un nouveau katakana
 					client.send(msg.encode())
 					print("Nouveau katakana envoyé")
 					reponse = client.recv(1024).decode()
 					print(reponse)
-					if reponse != 'STOP':
+					if reponse != 'STOP':  # Le client change d'exercice
 						if reponse == solution :
 							client.send("VRAI".encode())
 						else :
 							client.send(solution.encode())
 				if rep.decode() == "nouveauSigne" :
 					solution = choice(lettre_)
-					msg = lettre_to_signe[solution] #Envoi du nom du fichier image qui correspond au signe
+					msg = lettre_to_signe[solution]  # Envoit l'image du signe
 					client.send(msg.encode())
 					print("Nouveau signe envoyé")
 					reponse = client.recv(1024).decode()
 					print(reponse)
-					if reponse != 'STOP':
+					if reponse != 'STOP':  # Le client change d'exercice
 						if reponse == solution :
 							client.send("VRAI".encode())
 						else :
 							client.send(solution.encode())
-				if rep.decode() == "multi_hira" :
+				if rep.decode() == "multi_hira" :  # Mini-jeu multijoueur
 					print (rep.decode())
 					self.multi_joueur(client)
-				if rep.decode() == "score" :
+				if rep.decode() == "score" :  # Lancer le calcul du score
 					print(rep.decode())
 					print("Score ?")
 					self.calcul_gagnant(client)
-							
+				# Pour se déconnecter...
 				if rep.decode() == "DECONNECTION" or rep.decode() == "" :
 					"Fermeture de l'application"
-					print("Deconnetion de ",client)
+					print("Déconnexion de ",client)
 					self.list_client.remove(client)
 					client.shutdown(0)
 					client.close()
 					break
 			except : #socket.error:
-				print("Erreur dans choix")
+				print("Erreur dans le choix")
 				break
 				
-				
+	# Connexion client-serveur
 	def run(self):
 		host = ""
 		try :
 			port = int(sys.argv[1])
-			print ("Ecoute sur le port %s"%port)
+			print ("Écoute sur le port %s" %port)
 		except :
 			port = 8000
-			print ("Ecoute sur le port %s (valeur par défault)"%port)
+			print ("Écoute sur le port %s (valeur par défault)" %port)
 		self.serveur.bind((host,port))
 		self.serveur.listen(5)
 		while True :
@@ -210,7 +190,7 @@ class serveur():
 			print ("Connexion de ",info_client[0])
 			self.list_client.append(client)
 			self.list_client.append((client,info_client))
-			threading.Thread(target = self.choix , args = (client,)).start()
+			threading.Thread(target = self.choix, args = (client, )).start()
 
 
 serveur = serveur()
